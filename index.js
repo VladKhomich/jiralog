36a4399bdec5e570277319550a333217d5a0c815
Vlad Khomich <vladislav.builder@gmail.com>
2025-09-15 18:30:30 +0400

    Add "report" command to generate worklog reports for the past days

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const { jCommand } = require("./commands/jCommand");
 const { template } = require("./commands/template");
 const { git } = require("./commands/git");
 const { format } = require("./commands/format");
+const { report } = require("./commands/report");
 
 program.command("config").description("Configures tool").action(config);
 program
@@ -35,20 +36,20 @@ program.command("drop").description("Drops todays log").action(drop);
 program.command("remove").description("Removes log item").action(remove);
 program.command("edit").alias("e").description("Edits log item").action(edit);
 program.command("post")
-    .description("Posts log to jira server")
-    .option('-y', 'yesterday')
-    .option('-d, --date <year>-<month>-<day>/<month>-<day>/<day>', 'offset in past (in days)')
-    .option('-o, --offset <int>', 'offset in past (in days)')
-    .action(post);
+  .description("Posts log to jira server")
+  .option('-y', 'yesterday')
+  .option('-d, --date <year>-<month>-<day>/<month>-<day>/<day>', 'offset in past (in days)')
+  .option('-o, --offset <int>', 'offset in past (in days)')
+  .action(post);
 
 program
-    .command("template")
-    .description('Manages templates')
-    .option('-l', 'lists available templates')
-    .option('-r', 'removes selected template')
-    .option('-e', 'edits selected template')
-    .option('-d', 'resets to default templates')
-    .action(template);
+  .command("template")
+  .description('Manages templates')
+  .option('-l', 'lists available templates')
+  .option('-r', 'removes selected template')
+  .option('-e', 'edits selected template')
+  .option('-d', 'resets to default templates')
+  .action(template);
 
 program
   .command("j")
@@ -58,15 +59,22 @@ program
   .action(jCommand);
 
 program.command("git")
-    .option('-r, --repos', 'lists all tracked repositories')
-    .option('-d, --date <date>', 'date to get contributions')
-    .action(git)
+  .option('-r, --repos', 'lists all tracked repositories')
+  .option('-d, --date <date>', 'date to get contributions')
+  .action(git)
 
 program.command('format')
-    .description('manages formats of logging')
-    .option('-c, --check', 'checks provided formats')
-    .option('-d, --default', 'resets formats to default values')
-    .option('-e, --edit', 'edits formats')
-    .action(format)
+  .description('manages formats of logging')
+  .option('-c, --check', 'checks provided formats')
+  .option('-d, --default', 'resets formats to default values')
+  .option('-e, --edit', 'edits formats')
+  .action(format)
+
+program
+  .command("report")
+  .alias("r")
+  .description("Reports worklogs")
+  .option('-d, --days <number>', 'creates report for number of last days')
+  .action(report);
 
 program.parse();
